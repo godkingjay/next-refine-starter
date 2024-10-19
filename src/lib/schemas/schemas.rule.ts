4c1a8aae69098b2d428f4e307c508201feeb64d8
Jarrian <glorianagojar@gmail.com>
2024-10-20 07:47:53 +0800

    feat: Schema Validators

--- src/lib/schemas/schemas.rule.ts
+++ src/lib/schemas/schemas.rule.ts
@@ -1,17 +1,27 @@
+import birthdateValidator from "./rules/birthdate.validator.json";
+import codeValidator from "./rules/code.validator.json";
+import dateValidator from "./rules/date.validator.json";
 import emailValidator from "./rules/email.validator.json";
+import genderValidator from "./rules/gender.validator.json";
 import generalNameValidator from "./rules/general-name.validator.json";
 import nameValidator from "./rules/name.validator.json";
 import numberValidator from "./rules/number.validator.json";
 import passwordValidator from "./rules/password.validator.json";
 import textValidator from "./rules/text.validator.json";
 import urlValidator from "./rules/url.validator.json";
+import usernameValidator from "./rules/username.validator.json";
 
 export {
+    birthdateValidator,
+    codeValidator,
+    dateValidator,
     emailValidator,
+    genderValidator,
     generalNameValidator,
     nameValidator,
     numberValidator,
     passwordValidator,
     textValidator,
     urlValidator,
+    usernameValidator,
 };
--- src/lib/schemas/schemas.validate.ts
+++ src/lib/schemas/schemas.validate.ts
@@ -1,16 +1,22 @@
 import { formatNumber } from "@lib/format";
-import { isEmpty, isNotEmpty, isNumber } from "@lib/utils";
+import { concatenate, isEmpty, isNotEmpty, isNumber } from "@lib/utils";
 import validator from "validator";
 import { z } from "zod";
 import {
+    birthdateValidator,
+    codeValidator,
+    dateValidator,
     emailValidator,
+    genderValidator,
     generalNameValidator,
     nameValidator,
     numberValidator,
     passwordValidator,
     textValidator,
     urlValidator,
+    usernameValidator,
 } from "./schemas.rule";
+import dayjs from "dayjs";
 
 export const schemaValidateRequired = (
     name: string = "Field",
@@ -31,6 +37,379 @@ export const schemaValidateRequired = (
         ctx.addIssue({
             code: z.ZodIssueCode.custom,
             message: `${name} is required`,
+            path: path,
+        });
+    }
+};
+
+/**
+ * Validate date of form schemas
+ *
+ * @param {string} name - The name of the date
+ * @param {string} date - The date to validate
+ * @param {z.RefinementCtx} ctx - The context of the schema
+ * @param {(string | number)[]} path - The path of the schema
+ * @param {{ required?: boolean }} options - The options of the schema
+ * @param {Set<string>} set - The set of dates
+ * @returns {void}
+ */
+export const schemaValidateBirthdate = (
+    name: string = birthdateValidator.replace.values.name.default,
+    date: string | undefined,
+    ctx: z.RefinementCtx,
+    path: (string | number)[] = ["date"],
+    set?: Set<string> | null,
+    options?: {
+        required?: boolean;
+        min?: number;
+        max?: number;
+    }
+) => {
+    const isSet = {
+        min: isNumber(birthdateValidator.min.value),
+        max: isNumber(birthdateValidator.max.value),
+    };
+
+    options = {
+        required: false,
+        min: options?.min ?? birthdateValidator.replace.values.min.default,
+        max: options?.max ?? birthdateValidator.replace.values.max.default,
+        ...options,
+    };
+
+    options.min = formatNumber(options.min);
+    options.max = formatNumber(options.max);
+
+    if (isNotEmpty(date) && date) {
+        try {
+            const today = new Date();
+            const birthdate = new Date(date);
+
+            let age = today.getFullYear() - birthdate.getFullYear();
+            const month = today.getMonth() - birthdate.getMonth();
+
+            if (
+                month < 0 ||
+                (month === 0 && today.getDate() < birthdate.getDate())
+            ) {
+                age--;
+            }
+
+            if (
+                age < birthdateValidator.min.value &&
+                age < birthdateValidator.max.value &&
+                (age < options.min || age > options.max) &&
+                options.min &&
+                options.max &&
+                isSet.min &&
+                isSet.max
+            ) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: birthdateValidator.minMax.customMessage
+                        .replace(
+                            birthdateValidator.replace.values.name.variable,
+                            name
+                        )
+                        .replace(
+                            birthdateValidator.replace.values.min.variable,
+                            options.min.toString()
+                        )
+                        .replace(
+                            birthdateValidator.replace.values.max.variable,
+                            options.max.toString()
+                        ),
+
+                    path: path,
+                });
+            } else if (
+                age < birthdateValidator.min.value &&
+                age < options.min &&
+                options.min &&
+                isSet.min
+            ) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: birthdateValidator.min.customMessage
+                        .replace(
+                            birthdateValidator.replace.values.name.variable,
+                            name
+                        )
+                        .replace(
+                            birthdateValidator.replace.values.min.variable,
+                            options.min.toString()
+                        ),
+                    path: path,
+                });
+            } else if (
+                age > birthdateValidator.max.value &&
+                age > options.max &&
+                options.max &&
+                isSet.max
+            ) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: birthdateValidator.max.customMessage
+                        .replace(
+                            birthdateValidator.replace.values.name.variable,
+                            name
+                        )
+                        .replace(
+                            birthdateValidator.replace.values.max.variable,
+                            options.max.toString()
+                        ),
+                    path: path,
+                });
+            } else if (set && set.has(date)) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: birthdateValidator.duplicate.customMessage.replace(
+                        birthdateValidator.replace.values.name.variable,
+                        name
+                    ),
+                    path: path,
+                });
+            }
+        } catch (err) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: birthdateValidator.valid.message,
+                path: path,
+            });
+        }
+
+        if (
+            !date.match(
+                new RegExp(
+                    birthdateValidator.regex.value,
+                    birthdateValidator.regex.flags
+                )
+            )
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: birthdateValidator.regex.message,
+                path: path,
+            });
+        } else if (set && set.has(date)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: birthdateValidator.duplicate.message,
+                path: path,
+            });
+        }
+    } else if (options?.required && isEmpty(date)) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: birthdateValidator.required.message,
+            path: path,
+        });
+    }
+};
+
+/**
+ * Validate code of form schemas
+ *
+ * @param {string} name - The name of the code
+ * @param {string} code - The code to validate
+ * @param {z.RefinementCtx} ctx - The context of the schema
+ * @param {(string | number)[]} path - The path of the schema
+ * @param {(string | number)[]} set - The set of codes
+ * @param {{
+ * 		required?: boolean;
+ * 		minLength?: number;
+ * 		minLength?: number;
+ * }} options - The options of the schema
+ * @returns {void}
+ */
+export const schemaValidateCode = (
+    name: string = codeValidator.replace.values.name.default,
+    code: string | undefined,
+    ctx: z.RefinementCtx,
+    path: (string | number)[] = ["code"],
+    set?: Set<string>,
+    options?: {
+        required?: boolean;
+        min?: number;
+        max?: number;
+    }
+) => {
+    options = {
+        required: false,
+        min: options?.min && codeValidator.replace.values.min.default,
+        max: options?.max && codeValidator.replace.values.max.default,
+        ...options,
+    };
+
+    options.min = formatNumber(options.min);
+    options.max = formatNumber(options.max);
+
+    if (isNotEmpty(code) && code) {
+        if (
+            isNotEmpty(options.min) &&
+            options.min &&
+            code.length < options?.min
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: codeValidator.min.customMessage
+                    .replace(codeValidator.replace.values.name.variable, name)
+                    .replace(
+                        codeValidator.replace.values.min.variable,
+                        options.min.toString()
+                    ),
+                path: path,
+            });
+        } else if (
+            isNotEmpty(options.max) &&
+            options.max &&
+            code.length > options?.max
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: codeValidator.max.customMessage
+                    .replace(codeValidator.replace.values.name.variable, name)
+                    .replace(
+                        codeValidator.replace.values.max.variable,
+                        options.max.toString()
+                    ),
+                path: path,
+            });
+        } else if (
+            !code.match(
+                new RegExp(codeValidator.regex.value, codeValidator.regex.flags)
+            )
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: codeValidator.valid.customMessage.replace(
+                    codeValidator.replace.values.name.variable,
+                    name
+                ),
+                path: path,
+            });
+        } else if (set && set.has(code)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: codeValidator.duplicate.customMessage.replace(
+                    codeValidator.replace.values.name.variable,
+                    name
+                ),
+                path: path,
+            });
+        }
+    } else if (options?.required && isEmpty(code)) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: codeValidator.required.customMessage.replace(
+                codeValidator.replace.values.name.variable,
+                name
+            ),
+            path: path,
+        });
+    }
+};
+
+/**
+ * Validate date of form schemas
+ *
+ * @param {string} name - The name of the date
+ * @param {string} date - The date to validate
+ * @param {z.RefinementCtx} ctx - The context of the schema
+ * @param {(string | number)[]} path - The path of the schema
+ * @param {Set<string>} set - The set of dates
+ * @param {{ required?: boolean }} options - The options of the schema
+ * @returns {void}
+ */
+export const schemaValidateDate = (
+    name: string = dateValidator.replace.values.name.default,
+    date: string | undefined,
+    ctx: z.RefinementCtx,
+    path: (string | number)[] = ["date"],
+    set?: Set<string> | null,
+    options?: {
+        required?: boolean;
+        minDate?: string | null;
+        maxDate?: string | null;
+    }
+) => {
+    options = {
+        required: false,
+        minDate:
+            options?.minDate ?? dateValidator.replace.values.minDate.default,
+        maxDate:
+            options?.maxDate ?? dateValidator.replace.values.maxDate.default,
+        ...options,
+    };
+
+    options.minDate = dayjs(options.minDate).format(
+        dateValidator.replace.values.format.default
+    );
+    options.maxDate = dayjs(options.maxDate).format(
+        dateValidator.replace.values.format.default
+    );
+
+    if (isNotEmpty(date) && date) {
+        if (
+            !date.match(
+                new RegExp(dateValidator.regex.value, dateValidator.regex.flags)
+            )
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: dateValidator.valid.customMessage.replace(
+                    dateValidator.replace.values.name.variable,
+                    name
+                ),
+                path: path,
+            });
+        } else if (
+            isNotEmpty(options?.minDate) &&
+            options?.minDate &&
+            date < options.minDate
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: dateValidator.minDate.customMessage
+                    .replace(dateValidator.replace.values.name.variable, name)
+                    .replace(
+                        dateValidator.replace.values.minDate.variable,
+                        options.minDate
+                    ),
+                path: path,
+            });
+        } else if (
+            isNotEmpty(options?.maxDate) &&
+            options?.maxDate &&
+            date > options.maxDate
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: dateValidator.maxDate.customMessage
+                    .replace(dateValidator.replace.values.name.variable, name)
+                    .replace(
+                        dateValidator.replace.values.maxDate.variable,
+                        options.maxDate
+                    ),
+                path: path,
+            });
+        } else if (set && set.has(date)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: dateValidator.duplicate.customMessage.replace(
+                    dateValidator.replace.values.name.variable,
+                    name
+                ),
+                path: path,
+            });
+        }
+    } else if (options?.required && isEmpty(date)) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: dateValidator.required.customMessage.replace(
+                dateValidator.replace.values.name.variable,
+                name
+            ),
             path: path,
         });
     }
@@ -89,6 +468,69 @@ export const schemaValidateEmail = (
             code: z.ZodIssueCode.custom,
             message: emailValidator.required.customMessage.replace(
                 emailValidator.replace.values.name.variable,
+                name
+            ),
+            path: path,
+        });
+    }
+};
+
+/**
+ * Validate gender of form schemas
+ *
+ * @param {string} name - The name of the gender
+ * @param {string} gender - The gender to validate
+ * @param {z.RefinementCtx} ctx - The context of the schema
+ * @param {(string | number)[]} path - The path of the schema
+ * @param {Set<string>} set - The set of company sizes
+ * @param {{
+ * 	required?: boolean;
+ *  choices?: string[];
+ * }} options - The options of the schema
+ * @returns {void}
+ */
+export const schemaValidateGender = (
+    name: string = genderValidator.replace.values.name.default,
+    gender: string | undefined,
+    ctx: z.RefinementCtx,
+    path: (string | number)[] = ["gender"],
+    set?: Set<string>,
+    options?: {
+        required?: boolean;
+        choices?: string[];
+    }
+) => {
+    if (isNotEmpty(gender) && gender) {
+        if (
+            isNotEmpty(options?.choices) &&
+            options?.choices &&
+            !options.choices.includes(gender)
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: genderValidator.values.customMessage
+                    .replace(genderValidator.replace.values.name.variable, name)
+                    .replace(
+                        genderValidator.replace.values.choices.variable,
+                        concatenate(", ", ...options.choices)
+                    ),
+                path: path,
+            });
+        } else if (set && set.has(gender)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: genderValidator.duplicate.customMessage.replace(
+                    genderValidator.replace.values.name.variable,
+                    name
+                ),
+                path: path,
+            });
+        }
+    } else if (options?.required && isEmpty(gender)) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: genderValidator.required.customMessage.replace(
+                genderValidator.replace.values.name.variable,
                 name
             ),
             path: path,
@@ -640,6 +1082,106 @@ export const schemaValidateURL = (
                 name
             ),
             path: path,
+        });
+    }
+};
+
+/**
+ * Validate username of form schemas
+ *
+ * @param {string} name - The name of the username
+ * @param {string | undefined} username - The username to validate
+ * @param {z.RefinementCtx} ctx - The context of the schema
+ * @param {(string | number)[]} usernamePath - The path of the username
+ * @param {Set<string>} set - The set of usernames
+ * @param {{ required?: boolean }} options - The options of the schema
+ * @returns {void}
+ */
+export const schemaValidateUsername = (
+    name: string = usernameValidator.replace.values.name.default,
+    username: string | undefined,
+    ctx: z.RefinementCtx,
+    usernamePath: (string | number)[] = ["username"],
+    set?: Set<string>,
+    options?: {
+        required?: boolean;
+        min?: number;
+        max?: number;
+    }
+) => {
+    options = {
+        required: false,
+        min: usernameValidator.replace.values.min.default,
+        max: usernameValidator.replace.values.max.default,
+        ...options,
+    };
+
+    options.min = formatNumber(options.min);
+    options.max = formatNumber(options.max);
+
+    if (isNotEmpty(username) && username) {
+        if (username.length < options.min) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: usernameValidator.min.customMessage
+                    .replace(
+                        usernameValidator.replace.values.name.variable,
+                        name
+                    )
+                    .replace(
+                        usernameValidator.replace.values.min.variable,
+                        options.min.toString()
+                    ),
+                path: usernamePath,
+            });
+        } else if (username.length > options.max) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: usernameValidator.max.customMessage
+                    .replace(
+                        usernameValidator.replace.values.name.variable,
+                        name
+                    )
+                    .replace(
+                        usernameValidator.replace.values.max.variable,
+                        options.max.toString()
+                    ),
+                path: usernamePath,
+            });
+        } else if (
+            !username.match(
+                new RegExp(
+                    usernameValidator.regex.value,
+                    usernameValidator.regex.flags
+                )
+            )
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: usernameValidator.regex.customMessage.replace(
+                    usernameValidator.replace.values.name.variable,
+                    name
+                ),
+                path: usernamePath,
+            });
+        } else if (set && set.has(username)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: usernameValidator.duplicate.customMessage.replace(
+                    usernameValidator.replace.values.name.variable,
+                    name
+                ),
+                path: usernamePath,
+            });
+        }
+    } else if (options?.required && isEmpty(username)) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: usernameValidator.required.customMessage.replace(
+                usernameValidator.replace.values.name.variable,
+                name
+            ),
+            path: usernamePath,
         });
     }
 };
