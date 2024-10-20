4a2498a8696d36982b9805bb61886614d9f4ed4d
Jarrian <glorianagojar@gmail.com>
2024-10-20 08:51:23 +0800

    fix: Styling

--- src/components/auth-page/auth-login.tsx
+++ src/components/auth-page/auth-login.tsx
@@ -14,156 +14,165 @@ import toast from "react-hot-toast";
 import { CustomAuthPageProps } from ".";
 
 type AuthLoginProps = CustomAuthPageProps & {
-  type: "login";
+    type: "login";
 };
 
 const AuthLogin = (props: AuthLoginProps) => {
-  const { mutate: login, isLoading } = useLogin();
-  const [passwordType, setPasswordType] = useState("password");
-  const isDesktop2xl = useMediaQuery("(max-width: 1530px)");
+    const { mutate: login, isLoading } = useLogin();
+    const [passwordType, setPasswordType] = useState("password");
+    const isDesktop2xl = useMediaQuery("(max-width: 1530px)");
 
-  const togglePasswordType = () => {
-    setPasswordType((prevType) => (prevType === "text" ? "password" : "text"));
-  };
+    const togglePasswordType = () => {
+        setPasswordType((prevType) =>
+            prevType === "text" ? "password" : "text",
+        );
+    };
 
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-    const formData = Object.fromEntries(
-      new FormData(e.currentTarget).entries()
+        const formData = Object.fromEntries(
+            new FormData(e.currentTarget).entries(),
+        );
+
+        toast.loading("Logging in...", {
+            id: "login",
+        });
+
+        login(formData, {
+            onSuccess: (e) => {
+                if (e.success) {
+                    toast.success("Login Successful", {
+                        id: "login",
+                    });
+                    if (props?.onSignIn) {
+                        props?.onSignIn();
+                    } else {
+                        window.location.assign(e.redirectTo || "/dashboard");
+                    }
+                } else {
+                    toast.error("Invalid email or password", {
+                        id: "login",
+                    });
+                }
+            },
+            onError: () => {
+                toast.error("An unexpected error occurred.");
+            },
+        });
+    };
+
+    return (
+        <Card className="border">
+            <CardContent className="m-4">
+                <div className="flex w-full flex-col">
+                    <div className="mt-6 text-2xl font-bold text-default-900 2xl:mt-8 2xl:text-3xl">
+                        Hey, Hello 👋
+                    </div>
+                    <div className="mt-2 text-base leading-6 text-default-600 2xl:text-lg">
+                        Enter the information you entered while registering.
+                    </div>
+
+                    <form onSubmit={onSubmit} className="mt-8 2xl:mt-7">
+                        <div className="relative">
+                            <Input
+                                type="email"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                disabled={isLoading}
+                                className={cn("peer")}
+                            />
+                            <Label
+                                htmlFor="email"
+                                className={cn(
+                                    "absolute start-1 top-2 z-10 origin-[0] -translate-y-5 scale-75 transform rounded-t bg-background px-2 text-base text-default-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
+                                    {
+                                        "text-sm": isDesktop2xl,
+                                    },
+                                )}
+                            >
+                                Email
+                            </Label>
+                        </div>
+
+                        <div className="relative mt-6">
+                            <Input
+                                type={
+                                    passwordType === "password"
+                                        ? "password"
+                                        : "text"
+                                }
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                                disabled={isLoading}
+                                className={cn("peer")}
+                            />
+                            <Label
+                                htmlFor="password"
+                                className={cn(
+                                    "absolute start-1 top-2 z-10 origin-[0] -translate-y-5 scale-75 transform rounded-t bg-background px-2 text-base text-default-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
+                                    {
+                                        "text-sm": isDesktop2xl,
+                                    },
+                                )}
+                            >
+                                Password
+                            </Label>
+                            <div
+                                className="absolute right-1 top-1/2 -translate-y-1/2 cursor-pointer ltr:right-4 rtl:left-4"
+                                onClick={togglePasswordType}
+                            >
+                                {passwordType === "password" ? (
+                                    <Icon
+                                        icon="heroicons:eye"
+                                        className="h-4 w-4 text-default-400"
+                                    />
+                                ) : (
+                                    <Icon
+                                        icon="heroicons:eye-slash"
+                                        className="h-4 w-4 text-default-400"
+                                    />
+                                )}
+                            </div>
+                        </div>
+
+                        <div className="mb-6 mt-5 flex flex-wrap gap-2">
+                            <div className="flex flex-1 items-center gap-1.5">
+                                <Checkbox
+                                    className="mt-[1px] border-default-300"
+                                    id="isRemembered"
+                                />
+                                <Label
+                                    htmlFor="isRemembered"
+                                    className="cursor-pointer whitespace-nowrap text-sm text-default-600"
+                                >
+                                    Remember me
+                                </Label>
+                            </div>
+                            <Link
+                                href="#"
+                                className="flex-none text-sm text-primary"
+                            >
+                                Forget Password?
+                            </Link>
+                        </div>
+                        <Button className="mt-5 w-full" disabled={isLoading}>
+                            <ActionLoadingText
+                                hideLoadIcon
+                                isLoading={isLoading}
+                                labels={{
+                                    load: "Sign In",
+                                    loading: "Signing In...",
+                                }}
+                            />
+                        </Button>
+                    </form>
+                </div>
+            </CardContent>
+        </Card>
     );
-
-    toast.loading("Logging in...", {
-      id: "login",
-    });
-
-    login(formData, {
-      onSuccess: (e) => {
-        if (e.success) {
-          toast.success("Login Successful", {
-            id: "login",
-          });
-          if (props?.onSignIn) {
-            props?.onSignIn();
-          } else {
-            window.location.assign(e.redirectTo || "/dashboard");
-          }
-        } else {
-          toast.error("Invalid email or password", {
-            id: "login",
-          });
-        }
-      },
-      onError: () => {
-        toast.error("An unexpected error occurred.");
-      },
-    });
-  };
-
-  return (
-    <Card className="border">
-      <CardContent className="m-4">
-        <div className="flex w-full flex-col">
-          <div className="text-default-900 mt-6 text-2xl font-bold 2xl:mt-8 2xl:text-3xl">
-            Hey, Hello 👋
-          </div>
-          <div className="text-default-600 mt-2 text-base leading-6 2xl:text-lg">
-            Enter the information you entered while registering.
-          </div>
-
-          <form onSubmit={onSubmit} className="mt-8 2xl:mt-7">
-            <div className="relative">
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email"
-                disabled={isLoading}
-                className={cn("peer")}
-              />
-              <Label
-                htmlFor="email"
-                className={cn(
-                  "text-default-600 absolute start-1 top-2 z-10 origin-[0] -translate-y-5 scale-75 transform rounded-t bg-background px-2 text-base duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
-                  {
-                    "text-sm": isDesktop2xl,
-                  }
-                )}
-              >
-                Email
-              </Label>
-            </div>
-
-            <div className="relative mt-6">
-              <Input
-                type={passwordType === "password" ? "password" : "text"}
-                id="password"
-                name="password"
-                placeholder="Password"
-                disabled={isLoading}
-                className={cn("peer")}
-              />
-              <Label
-                htmlFor="password"
-                className={cn(
-                  "text-default-600 absolute start-1 top-2 z-10 origin-[0] -translate-y-5 scale-75 transform rounded-t bg-background px-2 text-base duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
-                  {
-                    "text-sm": isDesktop2xl,
-                  }
-                )}
-              >
-                Password
-              </Label>
-              <div
-                className="absolute right-1 top-1/2 -translate-y-1/2 cursor-pointer ltr:right-4 rtl:left-4"
-                onClick={togglePasswordType}
-              >
-                {passwordType === "password" ? (
-                  <Icon
-                    icon="heroicons:eye"
-                    className="text-default-400 h-4 w-4"
-                  />
-                ) : (
-                  <Icon
-                    icon="heroicons:eye-slash"
-                    className="text-default-400 h-4 w-4"
-                  />
-                )}
-              </div>
-            </div>
-
-            <div className="mb-6 mt-5 flex flex-wrap gap-2">
-              <div className="flex flex-1 items-center gap-1.5">
-                <Checkbox
-                  className="border-default-300 mt-[1px]"
-                  id="isRemembered"
-                />
-                <Label
-                  htmlFor="isRemembered"
-                  className="text-default-600 cursor-pointer whitespace-nowrap text-sm"
-                >
-                  Remember me
-                </Label>
-              </div>
-              <Link href="#" className="flex-none text-sm text-primary">
-                Forget Password?
-              </Link>
-            </div>
-            <Button className="mt-5 w-full" disabled={isLoading}>
-              <ActionLoadingText
-                hideLoadIcon
-                isLoading={isLoading}
-                labels={{
-                  load: "Sign In",
-                  loading: "Signing In...",
-                }}
-              />
-            </Button>
-          </form>
-        </div>
-      </CardContent>
-    </Card>
-  );
 };
 
 export default AuthLogin;
--- src/components/ui/card-snippet.tsx
+++ src/components/ui/card-snippet.tsx
@@ -6,68 +6,72 @@ import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible";
 import { Switch } from "@/components/ui/switch";
 import { hslToHex } from "@/lib/utils";
 import { useThemeStore } from "@/store";
-import { themes } from "@utils";
+import { themes } from "@lib/config";
 import { useTheme } from "next-themes";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 interface CardSnippetProps {
-  title?: string;
-  children: React.ReactNode;
-  code?: string;
+    title?: string;
+    children: React.ReactNode;
+    code?: string;
 }
 const CardSnippet = ({ title, code, children }: CardSnippetProps) => {
-  const [show, setShow] = useState(false);
-  const toggle = () => {
-    setShow(!show);
-  };
-  const { theme: mode } = useTheme();
-  const { theme: config, setTheme: setConfig } = useThemeStore();
-  const newTheme = themes.find((theme) => theme.name === config);
+    const [show, setShow] = useState(false);
+    const toggle = () => {
+        setShow(!show);
+    };
+    const { theme: mode } = useTheme();
+    const { theme: config, setTheme: setConfig } = useThemeStore();
+    const newTheme = themes.find((theme) => theme.name === config);
 
-  const hslPrimary = `hsla(${
-    newTheme?.cssVars[mode === "dark" ? "dark" : "light"][
-      "secondary-foreground"
-    ]
-  })`;
-  const hslPrimary2 = `hsla(${
-    newTheme?.cssVars[mode === "dark" ? "dark" : "light"].secondary
-  })`;
+    const hslPrimary = `hsla(${
+        newTheme?.cssVars[mode === "dark" ? "dark" : "light"][
+            "secondary-foreground"
+        ]
+    })`;
+    const hslPrimary2 = `hsla(${
+        newTheme?.cssVars[mode === "dark" ? "dark" : "light"].secondary
+    })`;
 
-  const hexPrimary = hslToHex(hslPrimary);
-  const hexPrimary2 = hslToHex(hslPrimary2);
-  return (
-    <Card>
-      <CardHeader className="flex flex-row items-center">
-        {title && (
-          <CardTitle className="flex-1 leading-normal"> {title}</CardTitle>
-        )}
-        {code && (
-          <div className="flex-none">
-            <Switch id="airplane-mode" onClick={toggle} />
-          </div>
-        )}
-      </CardHeader>
-      <CardContent>
-        {children}
-        <Collapsible open={show}>
-          <CollapsibleContent className="CollapsibleContent">
-            <SyntaxHighlighter
-              language="javascript"
-              className="mt-6 rounded-md text-sm"
-              style={atomOneDark}
-              customStyle={{
-                padding: "24px",
-                backgroundColor: mode !== "dark" ? hexPrimary : hexPrimary2,
-              }}
-            >
-              {`${code}`}
-            </SyntaxHighlighter>
-          </CollapsibleContent>
-        </Collapsible>
-      </CardContent>
-    </Card>
-  );
+    const hexPrimary = hslToHex(hslPrimary);
+    const hexPrimary2 = hslToHex(hslPrimary2);
+    return (
+        <Card>
+            <CardHeader className="flex flex-row items-center">
+                {title && (
+                    <CardTitle className="flex-1 leading-normal">
+                        {" "}
+                        {title}
+                    </CardTitle>
+                )}
+                {code && (
+                    <div className="flex-none">
+                        <Switch id="airplane-mode" onClick={toggle} />
+                    </div>
+                )}
+            </CardHeader>
+            <CardContent>
+                {children}
+                <Collapsible open={show}>
+                    <CollapsibleContent className="CollapsibleContent">
+                        <SyntaxHighlighter
+                            language="javascript"
+                            className="mt-6 rounded-md text-sm"
+                            style={atomOneDark}
+                            customStyle={{
+                                padding: "24px",
+                                backgroundColor:
+                                    mode !== "dark" ? hexPrimary : hexPrimary2,
+                            }}
+                        >
+                            {`${code}`}
+                        </SyntaxHighlighter>
+                    </CollapsibleContent>
+                </Collapsible>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default CardSnippet;
